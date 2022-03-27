c12efb7504d9d761745ac2ce1dcff894a46f72fd
darveshh <102459733+darveshh@users.noreply.github.com>
2022-03-27 15:55:15 +0700

    Add files via upload

--- plugins/speed.js
+++ plugins/speed.js
@@ -3,7 +3,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, DevMode }) => {
     try {
         let old = performance.now()
-        await m.reply('_Testing speed..._')
+        await m.reply('_𝐃𝐀𝐑𝐕𝐄𝐒𝐇 𝐁𝐎𝐓 𝐓𝐄𝐒𝐓 𝐊𝐄𝐂𝐄𝐏𝐀𝐓𝐀𝐍..._')
         let neww = performance.now()
         m.reply(neww - old + 'ms')
     } catch (e) {
